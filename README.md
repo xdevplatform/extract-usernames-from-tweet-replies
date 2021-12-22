7e74895fccc75cbb3e07e172dfb250dbbe607894
Aurelia Specker <aspecker@twitter.com>
2021-12-22 16:19:58 +0100

    Add README instructions

---
+++ README.md
@@ -0,0 +1,56 @@
+# Retrieve Tweet replies
+
+This Python script pulls all direct replies to a specified Tweet, extracts [user mentions](https://help.twitter.com/en/using-twitter/mentions-and-replies) from the replies, and returns a list of usernames ordered by most to least frequently mentioned in the replies.
+
+This script uses v2 of the Twitter API, and more specifically:
+* [Search Tweets](https://developer.twitter.com/en/docs/twitter-api/tweets/search/introduction)
+* [Tweets lookup](https://developer.twitter.com/en/docs/twitter-api/tweets/lookup/introduction)
+
+## Requirements 
+
+* Twitter developer account.
+* Developer Project and App with access to the v2 full-archive search endpoint: [GET /2/tweets/search/all](https://developer.twitter.com/en/docs/twitter-api/tweets/search/api-reference/get-tweets-search-all).
+
+You can read more about getting access to all of the above in [Twitter's developer docs](https://developer.twitter.com/en/docs/twitter-api/getting-started/getting-access-to-the-twitter-api).
+
+## Authentication & set up
+
+Rename `.env_example` to `.env` and add the credentials for your developer App in between the `""`.
+```
+TWITTER_CONSUMER_KEY=""
+TWITTER_CONSUMER_SECRET=""
+TWITTER_ACCESS_TOKEN=""
+TWITTER_ACCESS_TOKEN_SECRET=""
+TWITTER_BEARER_TOKEN=""
+```
+Don't forget to add `.env` to your `.gitignore` file to keep your credentials secure. 
+
+## Running the script 
+
+```
+$ python3 replies.py -t <TWEET-ID>
+```
+
+|Flags|   |   |   |
+|---|---|---|---|
+|`-t`|Tweet ID|Required|ID of the Tweet for which you want to pull data.|
+|`-h`|Help|Optional|View a list of available flags and a description for each of these.|
+|`-s`|Start time|Optional|The oldest UTC timestamp from which the replies will be provided. Format: YYYY-MM-DDTHH:mm:ssZ; for example: 2021-12-04T01:30:00Z. If unspecified, will default to returning replies from up to 30 days ago.|
+|`-e`|End time|Optional|The newest, most recent UTC timestamp to which the replies will be provided. Format: YYYY-MM-DDTHH:mm:ssZ; for example: 2021-12-04T01:30:00Z. If unspecified, will default to [now - 30 seconds].|
+|   |   |   |   |
+
+Run the following command to see a list of available flags: 
+```
+$ python3 replies.py -h
+```
+
+## Output
+
+### Returned in the command line interface
+* Ordered dictionary containing: a list of all usernames present in the replies and the number of times each username was mentioned. 
+* Number of replies to the Tweet.
+* Number of usernames mentioned in these replies.
+* Total request count made to the Search API to get these replies. 
+
+### Returned in a new file
+* `replies.txt` containing a list of Tweet IDs for the replies.
--- replies.py
+++ replies.py
@@ -1,4 +1,3 @@
-#Requires access to Full Archive Search
 import os
 import sys
 import argparse
@@ -7,44 +6,67 @@ import time
 
 from collections import Counter
 from dotenv import load_dotenv
+
 load_dotenv(verbose=True)  # Throws error if no .env file is found
 
-consumer_key=os.getenv("TWITTER_CONSUMER_KEY")
-consumer_secret=os.getenv("TWITTER_CONSUMER_SECRET")
-access_token=os.getenv("TWITTER_ACCESS_TOKEN")
-access_token_secret=os.getenv("TWITTER_ACCESS_TOKEN_SECRET")
-bearer_token=os.getenv("TWITTER_BEARER_TOKEN")
+consumer_key = os.getenv("TWITTER_CONSUMER_KEY")
+consumer_secret = os.getenv("TWITTER_CONSUMER_SECRET")
+access_token = os.getenv("TWITTER_ACCESS_TOKEN")
+access_token_secret = os.getenv("TWITTER_ACCESS_TOKEN_SECRET")
+bearer_token = os.getenv("TWITTER_BEARER_TOKEN")
 
 # Argparse for CLI options. Run `python3 replies.py -h` to see the list of arguments.
 parser = argparse.ArgumentParser()
-parser.add_argument("-t", "--tweet_id", required=True, help="ID of the Tweet for which you want to pull replies")
-parser.add_argument("-s", "--start_time", help="The oldest UTC timestamp from which the Tweets will be provided. Format: YYYY-MM-DDTHH:mm:ssZ; for example: 2021-12-04T01:30:00Z. If unspecified, will default to returning Tweets from up to 30 days ago.")
-parser.add_argument("-e", "--end_time", help="The newest, most recent UTC timestamp to which the Tweets will be provided. Format: YYYY-MM-DDTHH:mm:ssZ; for example: 2021-12-04T01:30:00Z. If unspecified, will default to [now - 30 seconds].")
+parser.add_argument(
+    "-t",
+    "--tweet_id",
+    required=True,
+    help="ID of the Tweet for which you want to pull replies",
+)
+parser.add_argument(
+    "-s",
+    "--start_time",
+    help="The oldest UTC timestamp from which the replies will be provided. Format: YYYY-MM-DDTHH:mm:ssZ; for example: 2021-12-04T01:30:00Z. If unspecified, will default to returning replies from up to 30 days ago.",
+)
+parser.add_argument(
+    "-e",
+    "--end_time",
+    help="The newest, most recent UTC timestamp to which the replies will be provided. Format: YYYY-MM-DDTHH:mm:ssZ; for example: 2021-12-04T01:30:00Z. If unspecified, will default to [now - 30 seconds].",
+)
 args = parser.parse_args()
+
 
 def bearer_oauth(r):
     r.headers["Authorization"] = f"Bearer {bearer_token}"
     r.headers["User-Agent"] = "v2FullArchiveSearchPython"
     return r
 
+
 def get_parameters():
-    params = {'query': f'conversation_id:{args.tweet_id}', 'tweet.fields': 'in_reply_to_user_id,author_id,conversation_id,entities', 'max_results': '500'} 
+    params = {
+        "query": f"conversation_id:{args.tweet_id}",
+        "tweet.fields": "in_reply_to_user_id,author_id,conversation_id,entities",
+        "max_results": "500",
+    }
     if args.start_time:
-        params.update(start_time = args.start_time)
+        params.update(start_time=args.start_time)
     if args.end_time:
-        params.update(end_time = args.end_time)
-    
-    return(params, args.tweet_id)
+        params.update(end_time=args.end_time)
+
+    return (params, args.tweet_id)
+
 
 def get_replies(parameters):
-    
+
     replies = []
 
     search_url = "https://api.twitter.com/2/tweets/search/all"
     request_count = 0
 
-    while True: 
-        response = requests.request("GET", search_url, auth=bearer_oauth, params=parameters)
+    while True:
+        response = requests.request(
+            "GET", search_url, auth=bearer_oauth, params=parameters
+        )
         if response.status_code != 200:
             raise Exception(response.status_code, response.text)
         response_payload = response.json()
@@ -57,57 +79,77 @@ def get_replies(parameters):
         if "next_token" not in meta:
             break
         next_token = meta["next_token"]
-        parameters.update(next_token = next_token)
+        parameters.update(next_token=next_token)
         time.sleep(1)
 
     return replies, request_count
 
+
 def get_author(tweet_id):
     tweet_lookup_url = f"https://api.twitter.com/2/tweets/{tweet_id}"
-    parameters = {'tweet.fields': 'author_id', 'expansions':'author_id', 'user.fields':'username'}
+    parameters = {
+        "tweet.fields": "author_id",
+        "expansions": "author_id",
+        "user.fields": "username",
+    }
 
-    response = requests.request("GET", tweet_lookup_url, auth=bearer_oauth, params=parameters)
-    
+    response = requests.request(
+        "GET", tweet_lookup_url, auth=bearer_oauth, params=parameters
+    )
+
     if response.status_code != 200:
         raise Exception(response.status_code, response.text)
-    
+
     response_payload = response.json()
     author_id = response_payload["data"]["author_id"]
     for user in response_payload["includes"]["users"]:
         author_username = user["username"]
 
-    return(author_id, author_username)
+    return (author_id, author_username)
+
 
 def get_usernames(author_id, replies):
-    counter = 0
     usernames = []
+    replies_ids = []
 
-    for reply in replies: 
+    for reply in replies:
         # Only include Tweets that are in direct reply to the original Tweet
         if reply["in_reply_to_user_id"] == author_id:
-            counter += 1
             for mention in reply["entities"]["mentions"]:
                 usernames.append(mention["username"])
-    
-    print("Direct response count:", counter)
+            replies_ids.append(reply["id"])
 
-    return(usernames)
+    return usernames, replies_ids
+
 
 def count_and_sort(usernames, author_username):
 
     ordered_usernames = Counter(usernames)
-    
-    #Remove mentions of original author from results
+
+    # Remove mentions of original author from results
     ordered_usernames.pop(f"{author_username}")
 
     return ordered_usernames
 
-if __name__ == '__main__':
+
+def results(ordered_usernames, replies_ids, request_count):
+
+    with open("replies.txt", "w") as output:
+        output.write(str(replies_ids))
+
+    print("============================")
+    print(ordered_usernames)
+    print("============================")
+    print("* Number of direct replies to original Tweet:", len(replies_ids))
+    print("* Number of usernames mentioned:", len(ordered_usernames))
+    print("* Total request count:", request_count)
+    print("============================")
+
+
+if __name__ == "__main__":
     parameters, original_tweet_id = get_parameters()
     replies, request_count = get_replies(parameters)
-    print("Request count:", request_count)
     author_id, author_username = get_author(original_tweet_id)
-    usernames = get_usernames(author_id, replies)
+    usernames, replies_ids = get_usernames(author_id, replies)
     ordered_usernames = count_and_sort(usernames, author_username)
-    print("Number of usernames mentioned:", len(ordered_usernames))
-    print("Ordered list of mentioned usernames:", ordered_usernames)
+    results(ordered_usernames, replies_ids, request_count)
